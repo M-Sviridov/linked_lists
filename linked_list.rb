9ebe61528cfde46dcc834de568439f0bfcf8d6d4
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-27 21:28:23 +1000

    Refactor code

--- linked_list.rb
+++ linked_list.rb
@@ -12,10 +12,10 @@ class LinkedList
   end
 
   def append(value)
+    new_node = Node.new(value)
     if @head_node.nil?
-      @head_node = @tail_node = Node.new(value)
+      @head_node = @tail_node = new_node
     else
-      new_node = Node.new(value)
       @tail_node.next_node = @tail_node = new_node
     end
   end
@@ -56,32 +56,24 @@ class LinkedList
   def pop
     nil if size < 1
 
-    current_node = @head_node
-    current_node = current_node.next_node until current_node.next_node == @tail_node
+    current_node = at(size - 2) # gets the node before the current tail node
+    current_node.next_node = nil
     @tail_node = current_node
-    @tail_node.next_node = nil
   end
 
-  def contains?(value)
-    current_node = @head_node
-    while current_node
-      return true if current_node.value == value
+  def contains?(value, current_node = @head_node)
+    return true if current_node.value == value
+    return false if current_node == tail
 
-      current_node = current_node.next_node
-    end
-    false
+    contains?(value, current_node.next_node)
   end
 
-  def find(value)
-    count = 0
-    current_node = @head_node
-    while current_node
-      return count if current_node.value == value
+  def find(value, current_node = @head_node, count = 0)
+    return count if current_node.value == value
+    return nil if current_node == tail
 
-      count += 1
-      current_node = current_node.next_node
-    end
-    nil
+    count += 1
+    find(value, current_node.next_node, count)
   end
 
   def to_s
@@ -94,39 +86,15 @@ class LinkedList
   end
 
   def insert_at(value, index)
+    new_node = Node.new(value)
+    current_node = at(index)
+    previous_node = at(index - 1)
     if index.zero?
-      new_node = Node.new(value, @head_node.next_node)
-      @head_node.next_node = new_node
-    elsif index == size - 1
-      current_node = @head_node
-      new_node = Node.new(value, @tail_node)
-      (index - 1).times do
-        current_node = current_node.next_node
-      end
-      current_node.next_node = new_node
+      new_node.next_node = current_node
+      @head_node = new_node
     else
-      current_node = @head_node
-      new_node = Node.new(value)
-      (index - 1).times do
-        current_node = current_node.next_node
-      end
-      new_node.next_node = current_node.next_node
-      current_node.next_node = new_node
+      previous_node.next_node = new_node
+      new_node.next_node = current_node
     end
   end
 end
-
-test = LinkedList.new
-test.append(3)
-test.prepend(12)
-test.append(10)
-test.append(21)
-test.prepend(1)
-test.pop
-test.to_s
-test.insert_at(100, 0)
-test.to_s
-test.insert_at(50, test.size - 1)
-test.to_s
-test.insert_at(75, 3)
-test.to_s
