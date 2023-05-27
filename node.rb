8e4e7e5f71851d90ffd0efb54eb0ba9f1c0f35e6
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-27 17:03:37 +1000

    Reformat the initialization of Node class

--- node.rb
+++ node.rb
@@ -2,12 +2,11 @@
 
 # class representing a node in a linked list data structure
 class Node
-  def initialize
-    @value = nil
-    @next_node = nil
+  attr_reader :value
+  attr_accessor :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
-
-  def value; end
-
-  def next_node; end
 end
