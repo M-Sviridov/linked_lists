38e9e55394bf0c83603830360348015130665288
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-27 19:32:57 +1000

    Add methods to print linked list, remove last element and return node at specified index

--- linked_list.rb
+++ linked_list.rb
@@ -16,8 +16,7 @@ class LinkedList
       @head_node = @tail_node = Node.new(value)
     else
       new_node = Node.new(value)
-      @tail_node.next_node = @tail_node =  new_node
-      # @tail_node = new_node
+      @tail_node.next_node = @tail_node = new_node
     end
   end
 
@@ -43,4 +42,46 @@ class LinkedList
   def tail
     @tail_node
   end
+
+  def at(index)
+    current_node = @head_node
+    index.times do
+      return nil if current_node.next_node.nil?
+
+      current_node = current_node.next_node
+    end
+    current_node
+  end
+
+  def pop
+    nil if size < 1
+
+    current_node = @head_node
+    until current_node.next_node == @tail_node
+      puts current_node.next_node.value
+      current_node = current_node.next_node
+    end
+    @tail_node = current_node
+    @tail_node.next_node = nil
+  end
+
+  def to_s
+    current_node = @head_node
+    while current_node
+      print "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
+      puts 'nil' if current_node.nil?
+    end
+  end
 end
+
+test = LinkedList.new
+test.append(3)
+test.prepend(12)
+test.append(10)
+test.append(21)
+test.prepend(1)
+test.pop
+test.to_s
+test.at(5)
+test.pop
