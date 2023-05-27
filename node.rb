b6deb7b6b6519c3c6ae52d97edee3c54006b8550
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-27 16:57:42 +1000

    Initialize the node class and methods

---
+++ node.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# class representing a node in a linked list data structure
+class Node
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
+
+  def value; end
+
+  def next_node; end
+end
