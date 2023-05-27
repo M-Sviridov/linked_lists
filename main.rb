bd54be943e0e8d2bbb23aa5fcf9718b129d01240
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-27 21:29:54 +1000

    Add method calls to main program to test if linked list and node classes are functional

---
+++ main.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require './linked_list'
+
+test = LinkedList.new
+test.append(3)
+test.prepend(12)
+test.append(10)
+test.append(21)
+test.prepend(1)
+test.pop
+test.to_s
+test.insert_at(100, 0)
+test.to_s
+test.insert_at(50, test.size - 1)
+test.to_s
+test.insert_at(75, 3)
+test.to_s
+test.insert_at(1000, 2)
+test.to_s
+test.pop
+test.to_s
+test.pop
+test.pop
+test.to_s
