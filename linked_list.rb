37db0c9a63705846b2bfe39d9f554ce365c00f1d
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-27 18:42:14 +1000

    Initialize linked list and add methods to manipulate the linked list structure

--- linked_list.rb
+++ linked_list.rb
@@ -1,8 +1,46 @@
 # frozen_string_literal: true
 
+require './node'
+
 # class representing a linked list data structure
 class LinkedList
+  attr_reader :head_node, :tail_node
+
   def initialize
-    @list = []
+    @head_node = nil
+    @tail_node = nil
+  end
+
+  def append(value)
+    if @head_node.nil?
+      @head_node = @tail_node = Node.new(value)
+    else
+      new_node = Node.new(value)
+      @tail_node.next_node = @tail_node =  new_node
+      # @tail_node = new_node
+    end
+  end
+
+  def prepend(value)
+    new_node = Node.new(value, @head_node)
+    @head_node = new_node
+  end
+
+  def size
+    count = 0
+    current_node = @head_node
+    while current_node
+      count += 1
+      current_node = current_node.next_node
+    end
+    count
+  end
+
+  def head
+    @head_node
+  end
+
+  def tail
+    @tail_node
   end
 end
